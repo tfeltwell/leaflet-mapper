d9b2313c1cc815e20a9cde702db5813d438518dd
Tom Feltwell <tfeltwell@gmail.com>
2015-07-07 16:03:00 +0100

    Cleaning up

---
+++ js/data.js
@@ -0,0 +1,59 @@
+var mainPoints = [
+	[53.22848, -0.54783,"Minerva Building",6],
+	[53.23197, -0.53736,"The Collection",6],
+	[53.23045, -0.54780,"Horse & Groom",1],
+	[53.22595, -0.55105,"Holiday Inn Express",0],
+	[53.22967, -0.54634,"Holiday Inn",0],
+	[53.22999, -0.54688,"DoubleTree Hilton",0],
+	[53.22961, -0.53609,"Premier Inn",0],
+	[53.23055, -0.55546, "Court 2",0]
+
+];
+
+var foodPoints = [
+	[53.22908, -0.54509,"Brayford Waterfront",3],
+	[53.22603, -0.54463,"Yoyos Chinese Buffet",3],
+	[53.21734, -0.54547,"Castlegate Curryhouse",3],
+	[53.23215, -0.53869,"Bombay Curryhouse",3],
+	[53.23538, -0.53840,"Thailand No. 1",3],
+	[53.23215, -0.53875,"Ribs 'n' Bibs",3],
+	[53.22750, -0.53869,"Sign of the Fish",3],
+	[53.23169, -0.54064,"The Cheese Society",3],
+	[53.22570, -0.54232,"Curtis\' of Lincoln Bakery",3],
+	[53.23112, -0.55055,"Sainsburys Local",7],
+	[53.23066, -0.54000,"Tesco Express",7],
+	[53.22434, -0.55536,"Morrison\'s Supermarket",7],
+	[53.22552, -0.54288,"Tesco Express",7]
+];
+
+var pubPoints = [
+	[53.22707, -0.54465,"Tower Bar",1],
+	[53.22625, -0.54198,"Treaty of Commerce",1],
+	[53.23403, -0.53887,"Widow Cullens Well",1],
+	[53.23435, -0.53860,"Magna Carta",1],
+	[53.23589, -0.54280,"The Victoria",1],
+	[53.23160, -0.54090,"The Tap &amp; Spile",1],
+	[53.23185, -0.53893,"The Strait and Narrow",1],
+	[53.21643, -0.54589,"The Golden Eagle",1],
+	[53.23223, -0.55034,"The West End Tap",1],
+	[53.23024, -0.53613,"The Jolly Brewer",1],
+	[53.22855, -0.54073,"Stokes Coffee",2],
+	[53.22932, -0.54114,"Coffee Aroma",2],
+	[53.23426, -0.53880,"Tea rooms",2]
+];
+
+var touristPoints = [
+	[53.23437, -0.53777,"Lincoln Cathedral",4],
+	[53.23457, -0.53969,"Lincoln Castle",4],
+	[53.23824, -0.54652,"Ellis Windmill",4],
+	[53.2378, -0.5534,"West Common",4],
+	[53.22838, -0.54753,"Cash point",5],
+	[53.22612, -0.54465,"Cash point",5],
+	[53.22948, -0.54054,"Cash point",5],
+	[53.22915, -0.54074,"Cash point",5],
+	[53.22747, -0.54093,"Cash point",5],
+	[53.22534, -0.54549,"Cash point",5],
+	[53.22761, -0.54365,"Swans",4],
+	[53.23003, -0.54462,"Co-op Pharmacy",8],
+	[53.23027, -0.54535,"Lloyds Pharmacy",8]
+];
--- js/map.js
+++ js/map.js
@@ -65,3 +65,28 @@ for (var i = 0; i < touristPoints.length; i++){
   marker.bindPopup(title);
   tourist.addLayer(marker);
 }
+
+// Controls to toggle layers
+$('#all').click( function() {  
+  if(layerControls[0]==false){
+    map.addLayer(food);map.addLayer(pub);map.addLayer(tourist);layerControls[0]=true;layerControls[1]=true;layerControls[2]=true;} 
+  else {map.removeLayer(food);map.removeLayer(pub);map.removeLayer(tourist);layerControls[0]=false;layerControls[1]=false;layerControls[2]=false;}
+  return false; } ); 
+
+$('#1').click( function() {  
+  if(layerControls[0]==false){
+    map.addLayer(food);layerControls[0]=true;} 
+  else {map.removeLayer(food); layerControls[0]=false;}
+  return false; } ); 
+
+$('#2').click( function() {  
+  if(layerControls[1]==false){
+    map.addLayer(pub);layerControls[1]=true;} 
+  else {map.removeLayer(pub); layerControls[1]=false;}
+  return false; } ); 
+
+$('#3').click( function() {  
+  if(layerControls[2]==false){
+    map.addLayer(tourist);layerControls[2]=true;} 
+  else {map.removeLayer(tourist); layerControls[2]=false;}
+    return false; } ); 
